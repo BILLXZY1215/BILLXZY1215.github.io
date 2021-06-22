f047de0441ffdba1fbd0e451701f2dd41f16c623
BILLXZY1215 <scyzx2@gmail.com>
2021-06-22 17:27:55 +0800

    public available

---
+++ public/component---src-pages-404-js-11a7d8a3facdc993c83b.js
@@ -0,0 +1,2 @@
+(self.webpackChunkgatsby_starter_blog=self.webpackChunkgatsby_starter_blog||[]).push([[883],{9602:function(t,e,a){"use strict";a.r(e);var n=a(5953),l=a(2752),s=a(2029);e.default=function(t){var e=t.data,a=t.location,o=e.site.siteMetadata.title;return n.createElement(l.Z,{location:a,title:o},n.createElement(s.Z,{title:"404: Not Found"}),n.createElement("h1",null,"404: Not Found"),n.createElement("p",null,"You just hit a route that doesn't exist... the sadness."))}}}]);
+//# sourceMappingURL=component---src-pages-404-js-11a7d8a3facdc993c83b.js.map
