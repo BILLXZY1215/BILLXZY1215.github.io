8438a779b765519369c1ffb305a2ddbb97c5ae86
BILLXZY1215 <scyzx2@gmail.com>
2021-06-24 11:26:49 +0800

    feat: about page

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,17 +1,120 @@
 import * as React from "react";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { Link, graphql } from "gatsby";
-import { ChakraProvider, Tabs, TabList, Tab } from "@chakra-ui/react";
+import {
+  ChakraProvider,
+  Tabs,
+  TabList,
+  Tab,
+  Text,
+  Divider,
+  Heading,
+  HStack,
+  Button,
+  Tooltip,
+  useClipboard,
+  useToast,
+} from "@chakra-ui/react";
 import Bio from "../components/bio";
 import Layout from "../components/layout";
 import Seo from "../components/seo";
+import { FaLinkedin, FaResearchgate, FaGithub } from "react-icons/fa";
 
 const About = () => {
+  const info = [
+    {
+      url:
+        "https://www.linkedin.com/in/%E6%B3%BD%E5%AE%87-%E7%86%8A-5940b01a2/",
+      name: "LinkedIn",
+      cs: "linkedin",
+      icon: <FaLinkedin />,
+    },
+    {
+      url: "https://www.researchgate.net/profile/Zeyu-Xiong-5/research",
+      name: "ResearchGate",
+      cs: "green",
+      icon: <FaResearchgate />,
+    },
+    {
+      url: "https://github.com/BILLXZY1215",
+      name: "GitHub",
+      cs: "blackAlpha",
+      icon: <FaGithub />,
+    },
+  ];
+  const Item = ({ item }) => {
+    const { hasCopied, onCopy } = useClipboard(item.url);
+    const toast = useToast();
+    useEffect(() => {
+      if (hasCopied) {
+        toast({ title: "Copied!", status: "success", position: "top" });
+      }
+    }, [hasCopied, toast]);
+    return (
+      <Tooltip label={item.url}>
+        <Button leftIcon={item.icon} colorScheme={item.cs} onClick={onCopy}>
+          {item.name}
+        </Button>
+      </Tooltip>
+    );
+  };
   return (
-    <>
-      <div>About</div>
-      <div>xxx</div>
-    </>
+    <ChakraProvider>
+      <Heading mb={2}>Intro</Heading>
+      <Text fontSize="md">
+        Hi, I'm Zeyu. you can call be Bill as well. I'm a final year student
+        from Univeristy of Nottigham, Ningbo China🚀, currently, I'm a front-end
+        engineer trainee in{" "}
+        <a href="https://taoyoumu.yashihq.com/" style={{ color: "red" }}>
+          yashi
+        </a>
+        ,{" "}
+        <a href="https://ufair.net.cn/" style={{ color: "orange" }}>
+          uFair
+        </a>{" "}
+        and a Research fellow in{" "}
+        <a href="https://unnc-ucc.github.io/" style={{ color: "blue" }}>
+          ucc
+        </a>
+        . Beside's programming, I enjoy rock music and basketball.
+      </Text>
+      <Divider mt={2} mb={2} />
+      <Heading mb={2}>Interest</Heading>
+      <Heading mt={2} size="lg">
+        Developer
+      </Heading>
+      <Text fontSize="lg">
+        Currently, as a developer, I'm mainly focus on{" "}
+        <strong>front-end</strong> development. I'm learning{" "}
+        <strong>JavaScript</strong>,<strong>React.js</strong> and{" "}
+        <strong>Vue.js</strong>, if you have some awesome projects that need to
+        develop or maintain, please let me know.
+      </Text>
+      <Heading mt={2} size="lg">
+        Researcher
+      </Heading>
+      <Text fontSize="lg">
+        As a Researcher, I'm interested in areas of{" "}
+        <strong>CV (Computer Vision)</strong> and{" "}
+        <strong>NLP (Natural Language Processing)</strong>, especially facial
+        detection and symbolic music applied in computer science. If you have
+        some projects related to these areas, please let me know. Besides, I'm
+        looking for a master project as well.
+      </Text>
+      <Heading mt={2} size="lg">
+        Music
+      </Heading>
+      <Text fontSize="lg">
+        For avocation, I enjoy rock music, especially from China mainland.
+      </Text>
+      <Divider mt={2} mb={2} />
+      <Heading mb={2}>Contact</Heading>
+      <HStack>
+        {info.map((k) => (
+          <Item item={k} />
+        ))}
+      </HStack>
+    </ChakraProvider>
   );
 };
 
